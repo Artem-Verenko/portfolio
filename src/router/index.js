dbcf5cdc1ce447ef546693615ab489b5a699af4c
Artem Verenko <artem.verenco@gmail.com>
2025-03-01 23:30:32 +0200

    switch to hash mode for router history in Vue application

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HeroSection from '../views/HeroSection.vue'
 import Blog from '../views/Blog.vue'
 import Books from '../views/Books.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',

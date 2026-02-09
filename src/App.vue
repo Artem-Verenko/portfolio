df09b1754bc6cf724db1085cfe9792bcc7e4ecf1
Artem Verenko <artem.verenco@gmail.com>
2026-02-09 02:42:38 +0200

    Fix background image bundling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navbar from './components/layout/Navbar.vue'
 import Footer from './components/layout/Footer.vue'
+import backgroundUrl from '@/assets/background.png'
 </script>
 
 <template>
   <div
-    class="flex flex-col min-h-screen bg-[var(--bg-main)] text-[var(--text-main)] bg-[url(@/assets/background.png)] bg-cover bg-fixed bg-center"
+    class="flex flex-col min-h-screen bg-[var(--bg-main)] text-[var(--text-main)] bg-cover bg-fixed bg-center"
+    :style="{ backgroundImage: `url(${backgroundUrl})` }"
   >
     <Navbar />
     <main class="grow">
